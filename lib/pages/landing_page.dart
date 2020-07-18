beb43911f199017db40c0541ccb3f1bf72105334
Wafa Waheeda Syed <WafaWaheedaS@users.noreply.github.com>
2020-07-18 18:59:04 +0300

    Add login button

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -15,14 +15,41 @@ class LandingPage extends StatelessWidget {
                 image: AssetImage('images/sistersofislam-qa-whitepink.png'),
               ),
               Padding(
-                  padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
-                  child: Text(
-                    'Sisters of Islam Qatar',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 30),
-                  )),
+                padding: EdgeInsets.fromLTRB(20, 70, 20, 20),
+                child: Text(
+                  'Sisters of Islam Qatar',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30),
+                ),
+              ),
+              Container(
+                height: 70,
+                margin: EdgeInsets.symmetric(horizontal: 70, vertical: 30),
+                alignment: Alignment.center,
+                child: Text(
+                  'LOG IN',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(25),
+                  color: Colors.white12,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(20, 0, 20, 20),
+                child: Text(
+                  'Don`t have an account yet? SIGN UP here',
+                  style: TextStyle(
+                      color: Colors.grey[200],
+                      fontWeight: FontWeight.normal,
+                      fontSize: 15),
+                ),
+              ),
             ],
           ),
         ),
