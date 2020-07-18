a6c7f9c88a6710eb8b94a16e1cd8116e6e5b402c
Wafa Waheeda Syed <WafaWaheedaS@users.noreply.github.com>
2020-07-18 19:24:13 +0300

    Adjust positioning of UI

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -15,7 +15,7 @@ class LandingPage extends StatelessWidget {
                 image: AssetImage('images/sistersofislam-qa-whitepink.png'),
               ),
               Padding(
-                padding: EdgeInsets.fromLTRB(20, 70, 20, 20),
+                padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
                 child: Text(
                   'Sisters of Islam Qatar',
                   style: TextStyle(
@@ -24,31 +24,31 @@ class LandingPage extends StatelessWidget {
                       fontSize: 30),
                 ),
               ),
-              Container(
-                height: 70,
-                margin: EdgeInsets.symmetric(horizontal: 70, vertical: 30),
-                alignment: Alignment.center,
-                child: Text(
-                  'LOG IN',
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
+              Padding(
+                padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
+                child: Container(
+                  height: 70,
+                  margin: EdgeInsets.symmetric(horizontal: 70, vertical: 30),
+                  alignment: Alignment.center,
+                  child: Text(
+                    'LOG IN',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25),
+                    color: Colors.white12,
                   ),
                 ),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(25),
-                  color: Colors.white12,
-                ),
               ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(20, 0, 20, 20),
-                child: Text(
-                  'Don`t have an account yet? SIGN UP here',
-                  style: TextStyle(
-                      color: Colors.grey[200],
-                      fontWeight: FontWeight.normal,
-                      fontSize: 15),
-                ),
+              Text(
+                'Don`t have an account yet? SIGN UP here',
+                style: TextStyle(
+                    color: Colors.grey[200],
+                    fontWeight: FontWeight.normal,
+                    fontSize: 15),
               ),
             ],
           ),
