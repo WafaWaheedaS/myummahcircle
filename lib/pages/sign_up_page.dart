32508bc8243fdba5fb5fd5d6468384556716fc10
Wafa Waheeda Syed <WafaWaheedaS@users.noreply.github.com>
2020-07-21 21:00:16 +0300

    Add login sign-up ui

--- lib/pages/sign_up_page.dart
+++ lib/pages/sign_up_page.dart
@@ -1,31 +1,121 @@
 import 'package:flutter/material.dart';
+import 'package:sisters_of_islam_qatar/pages/login_page.dart';
 
 class SignUpPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final logo = Hero(
+      tag: 'hero',
+      child: CircleAvatar(
+        backgroundColor: Colors.transparent,
+        radius: 48.0,
+        child: Image.asset('images/sistersofislam-qa-02.png'),
+      ),
+    );
+
+    final name = TextFormField(
+      autofocus: false,
+      initialValue: '',
+      decoration: InputDecoration(
+        hintText: 'Name',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+
+    final email = TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      autofocus: false,
+      initialValue: '',
+      decoration: InputDecoration(
+        hintText: 'Email',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+
+    final newPassword = TextFormField(
+      autofocus: false,
+      initialValue: '',
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'Set your Password',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+
+    final confirmPassword = TextFormField(
+      autofocus: false,
+      initialValue: '',
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'Re-enter Password',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+
+    final signUpButton = GestureDetector(
+      child: Container(
+        height: 70,
+        margin: EdgeInsets.symmetric(horizontal: 70, vertical: 30),
+        alignment: Alignment.center,
+        child: Text(
+          'SIGN UP',
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.white70,
+          ),
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(25),
+          color: Color(0xFF880E4F),
+        ),
+      ),
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => LoginPage()),
+      ),
+    );
+
+    final loginHere = FlatButton(
+      child: Text(
+        'Have an account already? Login Here',
+        style: TextStyle(color: Colors.black54),
+      ),
+      onPressed: () => Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => LoginPage()),
+      ),
+    );
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color(0xFF880E4F),
-        title: Image.asset(
-          'images/sistersofislam-qa-whitepink.png',
-          height: 50,
-        ),
+        title: Text('Sisters of Islam Qatar'),
         centerTitle: true,
       ),
-      body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          padding: EdgeInsets.only(left: 24.0, right: 24.0),
           children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Text(
-                'Create an account',
-                style: TextStyle(
-                    color: Colors.grey[900],
-                    fontWeight: FontWeight.bold,
-                    fontSize: 30),
-              ),
-            ),
-          ]),
+            logo,
+            SizedBox(height: 48.0),
+            name,
+            SizedBox(height: 48.0),
+            email,
+            SizedBox(height: 8.0),
+            newPassword,
+            SizedBox(height: 8.0),
+            confirmPassword,
+            SizedBox(height: 24.0),
+            signUpButton,
+            loginHere
+          ],
+        ),
+      ),
     );
   }
 }
