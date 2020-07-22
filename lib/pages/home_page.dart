bf2ecbf4ddd367362cbb5174695917a8ea540ab8
ashhadulislam@gmail.com <ashhadulislam@gmail.com>
2020-07-23 00:11:32 +0300

    changed the home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,11 +6,17 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color(0xFF880E4F),
-        title: Image.asset(
-          'images/sistersofislam-qa-whitepink.png',
-          height: 50,
-        ),
-        centerTitle: true,
+        title: Text("Search"),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                showSearch(
+                  context: context,
+                  delegate: DataSearch(),
+                );
+              })
+        ],
       ),
       body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -26,6 +32,100 @@ class HomePage extends StatelessWidget {
               ),
             ),
           ]),
+    );
+  }
+}
+
+class DataSearch extends SearchDelegate<String> {
+  final allSearch = [
+    "al fanar islamic center",
+    "education city mosque",
+    "qatar muslim islahi center",
+    "dr ishtiaq",
+    "session on dhul-hijjah",
+    "indian friends circle(jih)",
+    "girls youth circle",
+    "dr khadija tabassum",
+    "dr hajera bilquis",
+  ];
+
+  final recentSearch = [
+    "dr ishtiaq",
+    "session on dhul-hijjah",
+    "indian friends circle(jih)",
+  ];
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    // actions for app bar
+    return [
+      IconButton(
+          icon: Icon(Icons.clear),
+          onPressed: () {
+            query = "";
+          })
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    // leading icon on the left of the app bar
+    return IconButton(
+        icon: AnimatedIcon(
+          icon: AnimatedIcons.menu_arrow,
+          progress: transitionAnimation,
+        ),
+        onPressed: () {
+          close(
+            context,
+            null,
+          );
+        });
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // show some result based on the selection
+    return Center(
+      child: Container(
+        height: 100.0,
+        width: 100.0,
+        child: Card(
+          color: Colors.red,
+          child: Center(
+            child: Text(query),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // show suggestions while someone is typing
+    //get the query made by the user
+    final suggestionList = query.isEmpty
+        ? recentSearch
+        : allSearch.where((srch) => srch.startsWith(query)).toList();
+    return ListView.builder(
+      itemBuilder: (context, index) => ListTile(
+        onTap: () {
+          showResults(context);
+        },
+        leading: Icon(Icons.local_activity),
+        title: RichText(
+          text: TextSpan(
+              text: suggestionList[index].substring(0, query.length),
+              style:
+                  TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+              children: [
+                TextSpan(
+                    text: suggestionList[index].substring(query.length),
+                    style: TextStyle(color: Colors.grey))
+              ]),
+        ),
+      ),
+      itemCount: suggestionList.length,
     );
   }
 }
